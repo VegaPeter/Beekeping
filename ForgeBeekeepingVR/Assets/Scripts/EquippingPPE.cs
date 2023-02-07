9b78644a85487d22a93b189a8617c3920fd98aad
ForgifyEverything <aidan@forgeacademy.co.za>
2023-02-07 09:12:53 +0200

    Beekeeping | Forest Demo Scene 1 | Started Setting Up PPE

---
+++ ForgeBeekeepingVR/Assets/Scripts/EquippingPPE.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippingPPE : MonoBehaviour
+{
+    [SerializeField] GameObject ppeSuit;
+    [SerializeField] GameObject helmetMeshScreen;
+    [SerializeField] AudioSource zipSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ppeSuit.SetActive(false);
+            //play zipping sound
+            zipSound.Play();
+            //equip gloved hands (scary)
+            //lower beekeeping helmet mesh
+            while (helmetMeshScreen.transform.localPosition.y >= 3f)
+            {
+                helmetMeshScreen.transform.localPosition -= new Vector3(0, .5f, 0);
+            }
+        }
+    }
+}
