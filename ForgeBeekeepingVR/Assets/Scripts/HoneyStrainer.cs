c987763b028be829ad9d6d22e353c77da9a37740
ForgifyEverything <aidan@forgeacademy.co.za>
2023-02-08 11:03:35 +0200

    Beekeeping | Forest Demo Scene 1 | Worked On Strainer Interactions & Mechanics

--- ForgeBeekeepingVR/Assets/Scripts/HoneyStrainer.cs
+++ ForgeBeekeepingVR/Assets/Scripts/HoneyStrainer.cs
@@ -1,3 +1,4 @@
+using Oculus.Interaction.HandGrab;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,36 @@ public class HoneyStrainer : MonoBehaviour
 
     [SerializeField] private GameObject honeyInStrainer;
     [SerializeField] private GameObject honeyForSale;
+    [SerializeField] private GameObject strainingTeeth;
+    [SerializeField] private Transform targetHoneyPos;
     private BucketController bucketController;
+    private bool raiseHoney, strainerON;
+
+    private void FixedUpdate()
+    {
+        if (raiseHoney)
+        {
+            honeyInStrainer.transform.position = Vector3.MoveTowards(honeyInStrainer.transform.position, targetHoneyPos.position, 0.03f * Time.deltaTime);
+
+            if (honeyInStrainer.transform.localPosition.y >= targetHoneyPos.localPosition.y)
+            {
+                raiseHoney = false;
+                filledWithUnstrainedHoney = true;
+                Debug.Log("No longer raising honey");
+            }
+        }
+        else if (strainerON)
+        {
+            RotateTeeth();
+        }
+    }
 
     public void StartStrainer()
     {
         if (filledWithUnstrainedHoney)
         {
             StartCoroutine(StrainingHoney());
+            strainerON = true;
         }
         else
         {
@@ -29,34 +53,48 @@ public class HoneyStrainer : MonoBehaviour
             honeyInStrainer.SetActive(true);
             bucketController = other.GetComponent<BucketController>();
 
-            while(honeyInStrainer.transform.position.y <= 2f)
+            if (honeyInStrainer.transform.localPosition.y <= 2f)
             {
-                honeyInStrainer.transform.position += new Vector3(0, .3f, 0);
+                raiseHoney = true;
             }
             Debug.Log("Filled with honey");
-
-            filledWithUnstrainedHoney = true;
             bucketController.EmptyBucket();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        raiseHoney = false;
     }
 
     private IEnumerator StrainingHoney()
     {
         yield return new WaitForSeconds(45);
+
         strainedHoney = true;
         filledWithUnstrainedHoney = false;
+        honeyInStrainer.transform.localPosition = new Vector3(0, 0.196f, 0);
+
         SpawnStrainedHoney();
+
+        strainerON = false;
+
         StopCoroutine(StrainingHoney());
     }
 
     public void SpawnStrainedHoney()
     {
-        strainedHoney = false; 
+        strainedHoney = false;
         Instantiate(honeyForSale, new Vector3(106.872f, 30.763f, 87.819f), Quaternion.identity); //Felt lazy and just manually got its world coordinates. 
     }
 
     public void Debugger()
     {
-        Debug.Log("why does the button press itself?");
+        strainerON = true;
+    }
+
+    private void RotateTeeth()
+    {
+        strainingTeeth.transform.Rotate(0, 0f, 1f, Space.Self);
     }
 }
